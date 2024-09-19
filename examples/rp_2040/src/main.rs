15d810c4e1bd678573d9eea371dfc1cdc769be6a
Bailey Townsend <baileytownsend2323@gmail.com>
2024-09-19 18:48:41 -0500

    PIO and i2c imported on example

--- examples/rp_2040/src/main.rs
+++ examples/rp_2040/src/main.rs
@@ -3,18 +3,46 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::pac::spi::Spi;
+use embassy_rp::bind_interrupts;
+use embassy_rp::i2c::{self, Config, InterruptHandler};
+use embassy_rp::peripherals::I2C0;
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::{Common, Irq, Pio, PioPin, ShiftDirection, StateMachine};
 use embassy_time::{Duration, Timer};
-use hm01b0::HM01B0;
+use embedded_hal_async::i2c::I2c;
+use hm01b0::{DataBits, PictureSize, HM01B0};
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    I2C0_IRQ => InterruptHandler<I2C0>;
+});
+
+bind_interrupts!(struct PioIrqs {
+    PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    let sda = p.PIN_14;
-    let scl = p.PIN_15;
-    let hm01b0 = HM01B0::new(p.I2C0, sda, scl);
+    //Set up I2C to set settings for the camera
+    let sda = p.PIN_4;
+    let scl = p.PIN_5;
+    let mut i2c = i2c::I2c::new_async(p.I2C0, scl, sda, Irqs, Config::default());
+
+    //setup PIO to read data from the camera
+    // embassy_rp::pio::Config
+
+    let Pio {
+        mut common,
+        irq3,
+        mut sm0,
+        mut sm1,
+        mut sm2,
+        ..
+    } = Pio::new(p.PIO0, PioIrqs);
+
+    // let hm01b0 = HM01B0::new(i2c, p.PIO0, PictureSize::Size320x240, DataBits::Bits8);
 
     let delay = Duration::from_secs(1);
 
