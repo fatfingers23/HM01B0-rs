e4e6ec6c74e0d5a718d38a8539933e3a34ac234e
Bailey Townsend <baileytownsend2323@gmail.com>
2024-09-22 23:09:32 -0500

    i feel closer

--- examples/rp_2040/src/main.rs
+++ examples/rp_2040/src/main.rs
@@ -14,12 +14,14 @@ use embassy_rp::{bind_interrupts, dma, Peripheral};
 use embassy_time::{Duration, Timer};
 use fixed::prelude::ToFixed;
 use fixed_macro::types::U56F8;
+use heapless::String;
 use hm01b0::{DataBits, PictureSize, HM01B0};
 use pio::WaitSource::GPIO;
 use pio::{
     Assembler, InSource, JmpCondition, MovDestination, MovOperation, MovSource, SetDestination,
     WaitSource,
 };
+use pio_proc::pio_asm;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
@@ -29,6 +31,23 @@ bind_interrupts!(struct Irqs {
 bind_interrupts!(struct PioIrqs {
     PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
 });
+
+static REMAP: [char; 256] = [
+    '$', '$', '$', '$', '@', '@', '@', '@', 'B', 'B', 'B', 'B', '%', '%', '%', '8', '8', '8', '8',
+    '&', '&', '&', '&', 'W', 'W', 'W', 'M', 'M', 'M', 'M', '#', '#', '#', '#', '*', '*', '*', 'o',
+    'o', 'o', 'o', 'a', 'a', 'a', 'a', 'h', 'h', 'h', 'h', 'k', 'k', 'k', 'b', 'b', 'b', 'b', 'd',
+    'd', 'd', 'd', 'p', 'p', 'p', 'q', 'q', 'q', 'q', 'w', 'w', 'w', 'w', 'm', 'm', 'm', 'Z', 'Z',
+    'Z', 'Z', 'O', 'O', 'O', 'O', '0', '0', '0', 'Q', 'Q', 'Q', 'Q', 'L', 'L', 'L', 'L', 'C', 'C',
+    'C', 'C', 'J', 'J', 'J', 'U', 'U', 'U', 'U', 'Y', 'Y', 'Y', 'Y', 'X', 'X', 'X', 'z', 'z', 'z',
+    'z', 'c', 'c', 'c', 'c', 'v', 'v', 'v', 'u', 'u', 'u', 'u', 'n', 'n', 'n', 'n', 'x', 'x', 'x',
+    'x', 'r', 'r', 'r', 'j', 'j', 'j', 'j', 'f', 'f', 'f', 'f', 't', 't', 't', '/', '/', '/', '/',
+    '\\', '\\', '\\', '\\', '|', '|', '|', '(', '(', '(', '(', ')', ')', ')', ')', '1', '1', '1',
+    '{', '{', '{', '{', '}', '}', '}', '}', '[', '[', '[', '[', ']', ']', ']', '?', '?', '?', '?',
+    '-', '-', '-', '-', '_', '_', '_', '+', '+', '+', '+', '~', '~', '~', '~', '<', '<', '<', '>',
+    '>', '>', '>', 'i', 'i', 'i', 'i', '!', '!', '!', '!', 'l', 'l', 'l', 'I', 'I', 'I', 'I', ';',
+    ';', ';', ';', ':', ':', ':', ',', ',', ',', ',', '"', '"', '"', '"', '^', '^', '^', '`', '`',
+    '`', '`', '\'', '\'', '\'', '\'', '.', '.', '.', ' ',
+];
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -100,10 +119,8 @@ async fn main(_spawner: Spawner) {
     // assembler.wait(0, GPIO, hsync_pin, false);
     // assembler.bind(&mut wrap_source);
     // let pio_program = assembler.assemble_with_wrap(wrap_source, wrap_target);
-
-    use pio_proc::pio_asm;
+    // 2 (8 -1)
     let program_with_defines = pio_proc::pio_asm!(
-      ".wrap_target",
     "pull noblock",
     "wait 0 gpio 6",
     "wait 1 gpio 6",
@@ -111,9 +128,10 @@ async fn main(_spawner: Spawner) {
     "wait 1 gpio 7",
     "wait 0 gpio 7",
     "jmp y--, 4",
+    ".wrap_target",
     "mov x, osr",
     "wait 1 gpio 7",
-    "set y, 15",
+    "set y, 14",
     "wait 1 gpio 8",
     "wait 0 gpio 8",
     "jmp y--, 10",
@@ -125,69 +143,117 @@ async fn main(_spawner: Spawner) {
     ".wrap"
         options(max_program_size = 32) // Optional, defaults to 32
     );
-    let program = program_with_defines.program;
 
+    let test_program = pio_asm!(
+        ".wrap_target",
+        "in pins, 8",  // Read 8 bits from the input pins
+        ".wrap"
+        options(max_program_size = 32)
+    );
     // pio_program.wrap.target;
 
     // Load the PIO program into the PIO block
     let mut cfg = embassy_rp::pio::Config::default();
 
-    cfg.use_program(&common.load_program(&program), &[]);
+    cfg.use_program(&common.load_program(&program_with_defines.program), &[]);
 
     // cfg.clock_divider = (U56F8!(125_000_000) / U56F8!(10_000)).to_fixed();
-    cfg.clock_divider = 3.472.to_fixed();
+    // cfg.clock_divider = 1.25.to_fixed();
+    // cfg.clock_divider = 1.0.to_fixed();
     cfg.shift_in = ShiftConfig {
         auto_fill: true,
         threshold: 8,
         direction: ShiftDirection::Right,
     };
 
-    sm.set_config(&cfg);
-    sm.set_enable(true);
-    // Initialize GPIO pins for PIO
-    info!("After Pio");
-
     let vsync = common.make_pio_pin(p.PIN_6);
     let hsync = common.make_pio_pin(p.PIN_7);
     let pclk = common.make_pio_pin(p.PIN_8);
-    sm.set_pin_dirs(Direction::In, &[&vsync, &hsync, &pclk]);
-    cfg.set_in_pins(&[&common.make_pio_pin(p.PIN_9)]);
+    cfg.set_in_pins(&[&vsync, &hsync, &pclk, &common.make_pio_pin(p.PIN_9)]);
+
+    // sm.set_pin_dirs(Direction::In, &[&vsync, &hsync, &pclk]);
+
+    sm.set_config(&cfg);
+
+    // let vsync = common.make_pio_pin(p.PIN_6);
+    // let hsync = common.make_pio_pin(p.PIN_7);
+    // let pclk = common.make_pio_pin(p.PIN_8);
+    // sm.set_pin_dirs(Direction::In, &[&vsync, &hsync, &pclk]);
 
     // sm.set_pins()
     // Configure DMA
     // PIO and DMA configuration
     let mut dma_out_ref = p.DMA_CH0.into_ref();
-    let mut dma_in_ref = p.DMA_CH1.into_ref();
     let length = 160 * 120; // Assuming 160x120 image size for the example
-    let mut buffer: [u8; 160 * 120] = [0; 160 * 120];
-
+    let mut pixels: [u8; 160 * 120] = [0; 160 * 120];
+    let clear: [u8; 1120] = [0; 1120];
+    hm01b0.write_reg8(0x0100, 0x01).await.unwrap();
     loop {
-        sm.restart();
-        hm01b0.write_reg8(0x0100, 0x01).await.unwrap();
+        info!("Start of the loops");
+        sm.set_enable(true);
 
-        info!("before dma read");
+        // hm01b0.write_reg8(0x0601, 0x0601).await.unwrap();
 
-        let rx = sm.rx();
+        let (rx, tx) = sm.rx_tx();
+
+        let empty = rx.empty();
         info!("Empty: {:?}", rx.empty());
-        let idk = rx.try_pull();
-        info!("{:?}", idk);
+        info!("Full: {:?}", rx.full());
 
-        rx.dma_pull(dma_in_ref.reborrow(), &mut buffer).await;
-        info!("its working?");
-        info!("{:?}", buffer[0]);
+        if !empty {
+            match rx.try_pull() {
+                Some(data) => {
+                    // Extract relevant data from the word (adjust depending on pixel format)
+                    // info!("Received data: {:#010x}", data);
+                    let pixel_value = (data >> 24) & 0xFF;
+                    info!("Pixel Value: {:02x}", pixel_value);
+                }
+                None => {
+                    info!("FIFO read error");
+                }
+            }
+            rx.dma_pull(dma_out_ref.reborrow(), &mut pixels).await;
+            info!("Pixel 5: {:?}", pixels[5]);
 
-        for y in (0..120).step_by(2) {
-            // Map each pixel in the row to an ASCII character, and send the row over stdio
-            info!("\x1B[{}H", y / 2);
-            // let mut row = String::with_capacity(160);
+            for y in (0..120).step_by(2) {
+                // Map each pixel in the row to an ASCII character, and send the row over stdio
+                // info!("\x1B[{}H", y / 2);
+                let mut row = String::<160>::new();
+                // let mut row = String::with_capacity(160);
 
-            // for x in 0..160 {
-            //     let pixel = pixels[160 * y + x];
-            //     row.push(remap[pixel as usize]);
+                for x in 0..160 {
+                    let pixel = pixels[160 * y + x];
+                    row.push(REMAP[pixel as usize]);
+                }
+                info!("{}\x1B[K", row);
+                // stdout.flush().unwrap(); // Ensure the output is flushed immediately
+            }
+            info!("\x1B[J");
+            // for i in 0..pixels.len() {
+            //     info!("{:?}", pixels[i])
             // }
-            // print!("{}\x1B[K", row);
-            // stdout.flush().unwrap(); // Ensure the output is flushed immediately
+            // info!("{:?}", pixels[500]);
+        } else {
+            // tx.dma_push(dma_out_ref.reborrow(), &clear).await; // Push data via DMA
         }
-        // print!("\x1B[J");
+
+        // for y in (0..120).step_by(2) {
+        //     // Map each pixel in the row to an ASCII character, and send the row over stdio
+        //     // info!("\x1B[{}H", y / 2);
+        //     let mut row = String::<160>::new();
+        //     // let mut row = String::with_capacity(160);
+        //
+        //     for x in 0..160 {
+        //         let pixel = pixels[160 * y + x];
+        //         row.push(REMAP[pixel as usize]);
+        //     }
+        //     info!("{}\x1B[K", row);
+        //     // stdout.flush().unwrap(); // Ensure the output is flushed immediately
+        // }
+        // info!("\x1B[J");
+        sm.set_enable(false);
+        // hm01b0.write_reg8(0x0100, 0x00).await.unwrap();
+
+        // hm01b0.write_reg8(0x0100, 0x00).await.unwrap();
     }
 }
